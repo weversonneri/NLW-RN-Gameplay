ad67bb3643ae8362c943f51994d2aef302d98ce2
weversonneri <weversonneri@outlook.com>
2021-08-07 14:56:31 -0300

    :sparkles: validação de formulario

--- src/validations/appointmentCreateForm.ts
+++ src/validations/appointmentCreateForm.ts
@@ -4,8 +4,8 @@ export const schema = yup.object().shape({
   guild: yup.object().required(),
   category: yup.string().required(),
   day: yup.string().required(),
-  month: yup.string().required(),
-  hour: yup.string().required(),
-  minute: yup.string().required(),
+  month: yup.number().min(0).max(12).required(),
+  hour: yup.number().min(0).max(23).required(),
+  minute: yup.number().min(0).max(59).required(),
   description: yup.string().required()
 });
